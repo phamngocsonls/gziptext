253e7446d2ab94936d3fec11f9e7d20657a2ea16
Fujimoto Seiji <fujimoto@ceptord.net>
2016-10-03 18:20:20 +0900

    compiler.py: Add .gz to .gzt converter.

---
+++ gziptext/util.py
@@ -0,0 +1,33 @@
+
+import re
+import zlib
+
+
+def parse_bytes(buf):
+    if not re.match('".*"$', buf):
+        raise ValueError('value not quoted properly')
+    return list(bytearray.fromhex(buf[1:-1]))
+
+
+def parse_string(buf):
+    if not re.match('".*"$', buf):
+        raise ValueError('value not quoted properly')
+    s = ''
+    for c in buf[1:-1]:
+        if c == '\\':
+            continue
+        s += c
+    return s + '\0'
+
+
+def crc16(buf):
+    crc32 = zlib.crc32(buf)
+    return crc32 & 0xffff
+
+
+def wrapline(bstr, length=72):
+    """Wrap long lines."""
+    res = ''
+    for idx in range(0, len(bstr), length):
+        res += bstr[idx:idx+length] + '\n'
+    return res
